7c326579a92f4101071aa2dd563f57c8ffb4ba89
Brent Cromley <brentlyjr@yahoo.com>
2026-05-15 15:20:47 -0700

    Put URL in local configuration

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -28,7 +28,7 @@ export APNS_ENV=${APNS_ENV:-"development"}
 export APNS_KEY_ID=${APNS_KEY_ID:-"MU2W4LLRSY"}
 export APNS_TEAM_ID=${APNS_TEAM_ID:-"SVFXRTGAKU"}
 export APNS_BUNDLE_ID=${APNS_BUNDLE_ID:-"com.brentlyjr.WhatThePooh"}
-export WEBSOCKET_URL=${WEBSOCKET_URL:-"wss://themeparkswiki.herokuapp.com/v1/live"}
+
 export THEMEPARK_API_KEY=${THEMEPARK_API_KEY:-"519dd9c1-cc1e-4d4a-906d-d628cf0250bc"}
 
 # Set the APNS key path for local development (sandbox)
